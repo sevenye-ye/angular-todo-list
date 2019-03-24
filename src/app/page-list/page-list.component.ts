316ec4a974a446f4c71c3bbbb1a594deeffd9ab6
sevenye-ye <ylinylin777.gmail.com>
2019-03-24 12:05:31 +0100

    page-list.component.html && .sass && import ../saas/var

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrustedScriptString } from '@angular/core/src/sanitization/bypass';
 
 @Component({
   selector: 'app-page-list',
@@ -9,6 +10,7 @@ export class PageListComponent implements OnInit {
 
   public toDoShow: boolean;
   public toDoDoneShow: boolean;
+  public tryInit: any;
 
   constructor() { 
     this.toDoShow = true;
